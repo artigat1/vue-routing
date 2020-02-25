5572cf69d81226bd8552d13927117205e929b043
Steve Doggett <steve@orbitalwitness.com>
2020-02-25 08:47:52 +0000

    Setting up and loading routes
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/597654

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+﻿import User from './components/user/User.vue'
+import Home from './components/Home.vue'
+
+export const routes = [
+    { path: '', component: Home },
+    { path: '/user', component: User },
+]
