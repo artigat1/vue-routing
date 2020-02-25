566a5059181dcf04e48441a68396ee5085d7091a
Steve Doggett <steve@orbitalwitness.com>
2020-02-25 08:58:47 +0000

    Navigating with router links
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5976554#questions

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+﻿<template>
+    <ul class="nav nav-pills">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/user">User</router-link></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+
+</style>
