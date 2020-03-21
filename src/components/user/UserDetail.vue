92a036326cbf1f7e76b697d711bde728c9634579
Steve Doggett <artigat1@gmail.com>
2020-03-21 13:48:54 +0000

    Navigating to nested routes
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5976628#overview

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,3 +1,12 @@
 <template>
-    <h3>Some User Details</h3>
+    <div>
+        <h3>Some User Details</h3>
+        <hr/>
+        <p>User loaded has ID: {{ $route.params.id }}</p>
+    </div>
 </template>
+
+<script>
+    export default {
+    }
+</script>
