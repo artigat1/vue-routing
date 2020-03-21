5e79b80b2303da194849b4a7ca54ba8888bf3ba8
Steve Doggett <artigat1@gmail.com>
2020-03-21 13:25:45 +0000

    Fetching and using route parameters
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5976598#overview

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,12 +2,19 @@
     <div>
         <h1>The user page</h1>
         <hr/>
+        <p>Loaded ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go to home</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id,
+            }
+        },
+
         methods: {
             navigateToHome() {
                 this.$router.push('/')
