c37c6ab6d0b7ff201a3e96bf0b6a54d3c4dc941c
Steve Doggett <artigat1@gmail.com>
2020-04-06 09:22:34 +0100

    Passing the hash fragment
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5976658#overview

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -3,14 +3,30 @@
         <h3>Some User Details</h3>
         <hr/>
         <p>User loaded has ID: {{ $route.params.id }}</p>
-        <router-link 
-                tag="button" 
-                :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
-                class="btn btn-primary">Edit</router-link>
+        <router-link
+                :to="link"
+                class="btn btn-primary"
+                tag="button">Edit
+        </router-link>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: { 
+                        id: this.$route.params.id 
+                    },
+                    query: { 
+                        locale: 'en',
+                        q: 100 
+                    },
+                    hash: '#data'
+                }
+            }
+        }
     }
 </script>
--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -3,5 +3,7 @@
         <h3>Edit the User</h3>
         <p>locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
+        <div style="height:900px"/>
+        <p id="data">Some extra data</p>
     </div>
 </template>
