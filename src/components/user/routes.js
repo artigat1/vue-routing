823be81397a6f45594827607ebd1df68d6cf7fd8
Steve Doggett <artigat1@gmail.com>
2020-04-09 09:31:13 +0100

    Split the user routes into a module

---
+++ src/components/user/routes.js
@@ -0,0 +1,40 @@
+﻿const User = resolve => {
+    require.ensure(['./User.vue'], () => {
+        resolve(require('./User.vue'))
+    }, 'user')
+}
+
+const UserStart = resolve => {
+    require.ensure(['./UserStart.vue'], () => {
+        resolve(require('./UserStart.vue'))
+    }, 'user')
+}
+
+const UserEdit = resolve => {
+    require.ensure(['./UserEdit.vue'], () => {
+        resolve(require('./UserEdit.vue'))
+    }, 'user')
+}
+
+const UserDetail = resolve => {
+    require.ensure(['./UserDetail.vue'], () => {
+        resolve(require('./UserDetail.vue'))
+    }, 'user')
+}
+
+export const userRoutes = [
+    {
+        path: '/user', 
+        component: User,
+        children: [
+            { path: '', component: UserStart },
+            {
+                path: ':id', component: UserDetail, beforeEnter: (to, from, next) => {
+                    console.log('inside route setup')
+                    next()
+                }
+            },
+            { path: ':id/edit', component: UserEdit, name: 'userEdit' },
+        ]
+    },
+]
--- src/main.js
+++ src/main.js
@@ -1,34 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import { routes } from './routes'
+import { coreRoutes } from './routes'
+import { userRoutes } from './components/user/routes'
 import App from './App.vue'
 
 Vue.use(VueRouter)
 
+const routes = [
+    ...coreRoutes,
+    ...userRoutes,
+]
+
 const router = new VueRouter({
-  routes,
-  mode: 'history',
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    }
-    
-    if (to.hash) {
-      return { selector: to.hash }
-    }
-    
-    return { x: 0, y: 0 }
-  },
+    routes,
+    mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        }
+
+        if (to.hash) {
+            return { selector: to.hash }
+        }
+
+        return { x: 0, y: 0 }
+    },
 })
 
 router.beforeEach((to, from, next) => {
-  console.log('global beforeEach', to, from)
-  next()
+    console.log('global beforeEach', to, from)
+    next()
 })
 
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
