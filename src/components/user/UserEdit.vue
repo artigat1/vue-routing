152891a6aded4b08db057e8afbd3d0a9acaff2f3
Steve Doggett <artigat1@gmail.com>
2020-04-09 09:05:33 +0100

    Using the beforeLeave guard
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5976690#overview

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,8 +4,32 @@
         <p>locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
         <hr/>
-        <button class="btn btn-primary">Confirm</button>
+        <button @click="confirmed = true" class="btn btn-primary">Confirm</button>
         <div style="height:900px"/>
         <p id="data">Some extra data</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+        },
+
+        beforeRouteLeave(to, from, next) {
+            if (this.confirmed) {
+                next()
+            } else {
+                if (confirm('Are you sure?')) {
+                    next()
+                } else {
+                    next(false)
+                }
+            }
+        },
+
+        methods: {}
+    }
+</script>
